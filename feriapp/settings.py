0b06e26ab5265de92e4ec607f393c4e33613bcc6
Gustavo Ennes <gustavo_ennes@yahoo.com>
2020-06-19 15:30:57 -0300

    somar justificativas

--- feriapp/settings.py
+++ feriapp/settings.py
@@ -21,11 +21,11 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "lkl"
-#SECRET_KEY = os.environ['SECRET_KEY']
+#SECRET_KEY = "lkl"
+SECRET_KEY = os.environ['SECRET_KEY']
 
 
 # Application definition
