c36c3c37954ab7bb18352747970dc34076b5ad6c
Gustavo Ennes <gustavo_ennes@yahoo.com>
2020-07-14 10:49:51 -0300

    unique field= registro

--- app/models.py
+++ app/models.py
@@ -38,7 +38,7 @@ class Trabalhador(models.Model):
 
     nome = models.CharField(max_length=100, unique=True)
     matricula = models.CharField(unique=True, max_length=15)
-    registro = models.CharField(max_length=15)
+    registro = models.CharField(unique=True, max_length=15)
     funcao = models.CharField(max_length=50)
     setor = models.ForeignKey(Setor, on_delete=models.SET_NULL, null=True)
     data_admissao = models.DateTimeField()
