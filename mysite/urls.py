4509e31872320d3f2f4c153a70be90a81742a810
Gustavo Ennes <gustavo_ennes@yahoo.com>
2021-07-25 11:37:51 -0300

    threejs index file import changed

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,4 +19,4 @@ from mysite import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
